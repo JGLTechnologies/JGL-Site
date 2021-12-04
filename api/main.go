c8d8b88adbf728bc1f746089a0150a7c211bdab7
Nebulizer1213 <fixingg@gmail.com>
2021-12-03 20:42:08 -0500

    update

--- api/main.go
+++ api/main.go
@@ -10,27 +10,23 @@ import (
 	"net/http"
 )
 
+type postForm struct {
+	name    string `form:"name" binding:"required"`
+	email   string `form:"email" binding:"required"`
+	message string `form:"message" binding:"required"`
+	token   string `form:"token" binding:"required"`
+}
+
 func Contact(c *gin.Context) {
-	name, exists := c.GetPostForm("name")
-	if !exists {
-		c.JSON(400, gin.H{"error": "no name was specified"})
+	formData := postForm{}
+	if err := c.Bind(formData); err != nil {
+		c.JSON(400, gin.H{"error": "invalid request"})
 		return
 	}
-	email, exists := c.GetPostForm("email")
-	if !exists {
-		c.JSON(400, gin.H{"error": "no email was specified"})
-		return
-	}
-	message, exists := c.GetPostForm("message")
-	if !exists {
-		c.JSON(400, gin.H{"error": "no message was specified"})
-		return
-	}
-	token, exists := c.GetPostForm("token")
-	if !exists {
-		c.JSON(400, gin.H{"error": "no token was specified"})
-		return
-	}
+	name := formData.name
+	email := formData.email
+	message := formData.message
+	token := formData.token
 	data := map[string]string{"name": name, "email": email, "message": message, "token": token, "ip": utils.GetIP(c)}
 	jsonData, _ := json.Marshal(data)
 	res, err := http.Post("https://jglbotapi.us/contact", "application/json", bytes.NewBuffer(jsonData))
