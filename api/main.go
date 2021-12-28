334a93f3385c3413e9931b6f6bb7a4f1db5bb8ad
Nebulizer1213 <fixingg@gmail.com>
2021-12-28 17:37:59 -0500

    update

--- api/main.go
+++ api/main.go
@@ -19,7 +19,7 @@ type postForm struct {
 func Contact(c *gin.Context) {
 	formData := postForm{}
 	if bindingErr := c.Bind(&formData); bindingErr != nil {
-		c.HTML(400, "status", gin.H{"code": "400", "message": "Invalid request: missing field"})
+		c.HTML(400, "status", gin.H{"code": "400", "message": "Invalid request body"})
 		return
 	}
 	name := formData.Name
@@ -27,7 +27,7 @@ func Contact(c *gin.Context) {
 	message := formData.Message
 	token := formData.Token
 	if len(name) > 200 || len(email) > 254 || len(message) > 1020 {
-		c.HTML(400, "status", gin.H{"code": "400", "message": "Invalid request: string has exceeded the limit"})
+		c.HTML(400, "status", gin.H{"code": "400", "message": "Invalid request body"})
 		return
 	}
 	data := map[string]string{"name": name, "email": email, "message": message, "token": token, "ip": utils.GetIP(c)}
