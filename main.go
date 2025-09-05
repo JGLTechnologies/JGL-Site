ffac73386ae0263ee0b1a13e565b3f955a063f44
Nebulizer1213 <fixingg@gmail.com>
2025-09-05 15:26:57 -0400

    update

--- main.go
+++ main.go
@@ -106,15 +106,6 @@ func main() {
 	}
 
 	apiGroup := router.Group("/api")
-	{
-		apiGroup.GET("/bot/status", cache.CacheByRequestPath(store, time.Second*5), api.BotStatus)
-		apiGroup.GET("/bot/info", cache.CacheByRequestPath(store, time.Second*5), api.BotInfo)
-		apiGroup.POST("/traffic", cache.CacheByRequestPath(store, time.Second*5), api.CFProxy)
-		apiGroup.OPTIONS("/traffic", cache.CacheByRequestPath(store, time.Second*5), api.CFProxy)
-		apiGroup.POST("/contact", utils.GetMW(time.Second, 1), reqIDMiddleware, api.Contact)
-		apiGroup.GET("/error", cache.CacheByRequestURI(store, cacheTime), api.GetErr)
-	}
-
 	apiGroup.Use(func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
@@ -127,6 +118,14 @@ func main() {
 		}
 		c.Next()
 	})
+	{
+		apiGroup.GET("/bot/status", cache.CacheByRequestPath(store, time.Second*5), api.BotStatus)
+		apiGroup.GET("/bot/info", cache.CacheByRequestPath(store, time.Second*5), api.BotInfo)
+		apiGroup.POST("/traffic", cache.CacheByRequestPath(store, time.Second*5), api.CFProxy)
+		apiGroup.OPTIONS("/traffic", cache.CacheByRequestPath(store, time.Second*5), api.CFProxy)
+		apiGroup.POST("/contact", utils.GetMW(time.Second, 1), reqIDMiddleware, api.Contact)
+		apiGroup.GET("/error", cache.CacheByRequestURI(store, cacheTime), api.GetErr)
+	}
 
 	router.NoRoute(noRoute)
 	router.NoMethod(noMethod)
